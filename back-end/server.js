86a2e8be3fb4b8a6594f6a06da76859dd1bb5dde
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-07-31 11:22:45 -0300

    feat: criado o back-end, possui apenas uma api simples que retorna um JSON de livros(apenas durante os testes, sera mudado futuramente)

---
+++ back-end/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3001;
+
+const books = [
+	{ id: 1, title: 'O Senhor dos Anéis', author: 'J.R.R. Tolkien'},
+	{ id: 2, title: 'Duna', author: 'Frank Herbert'},
+	{ id: 3, title: 'Fundação', author: 'Isaac Asimov'},
+	{ id: 4, title: 'O Homem do Castelo Alto', author: 'Philip K Dick'},
+	{ id: 5, title: '1984', author: 'George Orwell'},
+	{ id: 6, title: 'Admirável Mundo Novo', author: 'Aldous Huxley'}
+];
+
+app.get('/', (req, res) => {
+  return res
+    .status(200)
+    .json(books)
+})
+
+app.listen(PORT, () => {
+  console.log(`Aplicação ouvindo na porta ${PORT}`);
+});
