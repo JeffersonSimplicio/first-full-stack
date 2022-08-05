81cf89a0501b0d6fd4553790620accc36b90df16
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-08-05 13:44:20 -0300

    Refactor: o back-end agora usa funções externas para fazer o CRUD

--- backend/server.js
+++ backend/server.js
@@ -1,55 +1,52 @@
 const express = require('express');
 const cors = require('cors');
+const { fileReader, addNewTask, taskEditor, deleteTask } = require('./service/crud')
+require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const PORT = 3001;
-let ids = 0;
-
-const toDoList = [];
+const HTTP_OK = Number(process.env.HTTP_OK_STATUS); // 200
+const HTTP_CREATED = Number(process.env.HTTP_CREATED_STATUS); // 201
+ // 204 NÃO PERMITE RESPOSTA
+const HTTP_NO_CONTENT = Number(process.env.HTTP_NO_CONTENT_STATUS); // 204
+const API_PORT = Number(process.env.API_PORT) || 3001;
+const DBSimulator = '../toDoList.json'
 // [{id: 123, task: "exemplo de tarefa"}]
 
-app.get('/to-do', (req, res) => {
+app.get('/to-do', async (_req, res) => {
+  const toDoList = await fileReader(DBSimulator)
   return res
-    .status(200)
+    .status(HTTP_OK)
     .json(toDoList);
 });
 
-const addTask = (task) => {
-  const taskWithId = {
-    id: ids + 1,
-    task
-  }
-  ids += 1;
-  return taskWithId;
-};
-
-app.post('/to-do/create', (req, res) => {
+app.post('/to-do/create', async (req, res) => {
   const { newTask } = req.body;
-  const taskObject = addTask(newTask);
-  toDoList.push(taskObject);
+  const taskObject = await addNewTask(DBSimulator, newTask);
   res
-    .status(201)
+    .status(HTTP_CREATED)
     .json(taskObject);
 });
 
-app.put('/to-do/edit', (req, res) => {
-  const { editedTask } = req.body;
-  const taskIndex = toDoList.findIndex((task) => task.id === Number(editedTask.id));
-  toDoList[taskIndex] = editedTask;
-  res.status(204).end();
+app.put('/to-do/edit/', async (req, res) => {
+  const editedTask = req.body;
+  const result = await taskEditor(DBSimulator, editedTask);
+  // WIP: trabalhar o renorno em caso de id não encontrado
+  // if (validation === -1) {
+  //   erro
+  // }
+  res.status(HTTP_OK).json(result);
 });
 
-app.delete('/to-do/delete/:id', (req, res) => {
+app.delete('/to-do/delete/:id', async (req, res) => {
   const { id } = req.params;
-  const taskIndex = toDoList.findIndex((task) => task.id === Number(id));
-  toDoList.splice(taskIndex, 1);
-  res.status(204).end();
+  await deleteTask(DBSimulator, id);
+  res.status(HTTP_NO_CONTENT).end();
 });
 
-app.listen(PORT, () => {
-  console.log(`Aplicação ouvindo na porta ${PORT}`);
+app.listen(API_PORT, () => {
+  console.log(`Aplicação ouvindo na porta ${API_PORT}`);
 });
